f50a817d6f491ac5ff02fc998d3b6e1c999753a1
Đinh Thiên Ân <134404922+dinhthienan33@users.noreply.github.com>
2024-05-01 17:46:21 +0700

    v1

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from factorial import fact
+def main():
+    st.title('Factorial App')
+    number=st.number_input('Enter a number',min_value=1,max_value=1000)
+    if st.button('Calculate'):
+        result=fact(number)
+        st.write(result)
+        st.balloons()
+if __name__ == '__main__':
+    main()
